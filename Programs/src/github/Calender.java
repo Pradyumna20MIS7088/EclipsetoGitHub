c50c09afb2c9024831518445e02fcfa942d9d418
pradyumna <pradyumna.20mis7088@vitap.ac.in>
2021-04-14 15:58:23 +0530

    Added Calender class

---
+++ Programs/src/github/Calender.java
@@ -0,0 +1,8 @@
+package github;
+import java.util.Calendar;
+public class Calender {
+    public static void main(String[] args) {
+        Calendar cal1= Calendar.getInstance();
+        System.out.println(cal1.getTime());
+    }
+}
